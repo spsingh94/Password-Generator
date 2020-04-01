06d71a8f02bccd0b833918d3800d0377df3bd70f
Shaminder Singh <spsingh94.ss@gmail.com>
2020-04-01 12:18:05 -0400

    updated assignment, trying to figure out how to use less if statements

--- Develop/script.js
+++ Develop/script.js
@@ -1,147 +1,174 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// get password area
+var passwordText = document.querySelector("#password");
+// make this variable equal to the function that you want to connect it with
+// var password = clickButton;
+
+
+//target the password area to update whats inside. make it equal to the variable that is connected
+//to your function. this should allow you to update text from your function to the variable which is connected
+//to the password area.
+
+//passwordText.value (our text area/password display) is equal to password. password is equal to our function.
+// passwordText.value = clickButton;
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
-
-function generatePassword() {
-  // prompt the user 2 questions (cap letter?, number?)
-  var capQuest = prompt("Would you like to use capital letters?");
-  var numQuest = prompt("Would you like to use numbers");
-  var lowerQuest = prompt("Would you like to use lower case letters?")
-  var charQuest = prompt("Would you like to use special characters?")
-  // make conditionals for the 4 possible scenarios (yy, yn, ny, nn)
-
-
-  // yes yes yes yes
-
-  if ("yes" + "yes" + "yes" + "yes") {
-    return (randomCapLetter() + randomCapLetter() + randomNum() + randomNum() +
-      randomLowerLetter() + randomLowerLetter() + randomChar() + randomChar())
-  }
-
-  // yes yes yes no
-
-  if ("yes" + "yes" + "yes" + "no") {
-    return (randomCapLetter() + randomCapLetter() + randomCapLetter() + randomNum() +
-      randomNum() + randomLowerLetter() + randomLowerLetter() + randomLowerLetter())
-  }
-
-  // yes yes no yes
-
-  if ("yes" + "yes" + "no" + "yes") {
-    return (randomCapLetter() + randomCapLetter() + randomCapLetter() + randomNum() +
-      randomNum() + randomChar() + randomChar() + randomChar())
-  }
-
-  // yes no yes yes
-
-  if ("yes" + "no" + "yes" + "yes") {
-    return (randomCapLetter() + randomCapLetter() + randomCapLetter() + randomChar() +
-      randomChar() + randomLowerLetter() + randomLowerLetter() + randomLowerLetter())
-  }
-
-  // no yes yes yes
-
-  if ("no" + "yes" + "yes" + "yes") {
-    return (randomNum() + randomNum() + randomNum() + randomChar() + randomChar() +
-      randomLowerLetter() + randomLowerLetter() + randomLowerLetter())
-  }
-
-  // yes yes no no
-
-  if ("yes" + "yes" + "no" + "no") {
-    return (randomCapLetter() + randomCapLetter() + randomCapLetter() +
-      randomCapLetter() + randomNum() + randomNum() + randomNum() + randomNum())
-
-  }
-
-  // yes no no yes 
-
-  if ("yes" + "no" + "no" + "yes") {
-    return (randomCapLetter() + randomCapLetter() + randomCapLetter() +
-      randomCapLetter() + randomChar() + randomChar() + randomChar() + randomChar())
-  }
-
-  // no no yes yes
-
-  if ("no" + "no" + "yes" + "yes") {
-    return (randomLowerLetter() + randomLowerLetter() + randomLowerLetter() +
-      randomLowerLetter() + randomChar() + randomChar() + randomChar() + randomChar())
-  }
-
-  // no yes yes no
-
-  if ("no" + "yes" + "yes" + "no") {
-    return (randomNum() + randomNum() + randomNum() + randomNum() + randomLowerLetter() +
-      randomLowerLetter() + randomLowerLetter() + randomLowerLetter())
-  }
-
-  // yes no no no
-
-  if ("yes" + "no" + "no" + "no") {
-    return (randomCapLetter() + randomCapLetter() + randomCapLetter() + randomCapLetter() +
-      randomCapLetter() + randomCapLetter() + randomCapLetter() + randomCapLetter())
-  }
-
-  // no no no yes 
-
-  if ("no" + "no" + "no" + "yes") {
-    return (randomChar() + randomChar() + randomChar() + randomChar() + randomChar() +
-      randomChar() + randomChar() + randomChar())
-  }
-
-  // no yes no no 
-
-  if ("no" + "yes" + "no" + "no") {
-    return (randomNum() + randomNum() + randomNum() + randomNum() +
-      randomNum() + randomNum() + randomNum() + randomNum())
-  }
-
-  // no no yes no
-
-  if ("no" + "no" + "yes" + "no") {
-    return (randomLowerLetter() + randomLowerLetter() + randomLowerLetter() + randomLowerLetter() +
-      randomLowerLetter() + randomLowerLetter() + randomLowerLetter() + randomLowerLetter())
-  }
-
-  // no no no no
-
-  if ("no" + "no" + "no" + "no") {
-    alert("ERROR! Must make a selection to generate password.")
-  }
-
-}
-
+var charQuestion = prompt("how many characters would you like the password to be?");
+var capAnswer = "";
+// capAnswer = charQuestion;
+// var capNumArray = [];
 // create random upper case letter array
 function randomCapLetter() {
-  var capLetterArr = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-  return capLetterArr[Math.floor(Math.random() * capLetterArr.length)]
+
+  var capLetterArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+    "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  for (var i = 0; i < charQuestion; i++) {
+    capAnswer += capLetterArr[Math.floor(Math.random() * capLetterArr.length)]
+  }
+  return capAnswer;
+  // return capLetterArr[Math.floor(Math.random() * capLetterArr.length)]
 }
 
 // create random number array 
 function randomNum() {
-  var numArr = ("0123456789")
-  return numArr[Math.floor(Math.random() * numArr.length)]
+  var numArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  for (var i = 0; i < charQuestion; i++) {
+    capAnswer += numArr[Math.floor(Math.random() * numArr.length)]
+  }
+  return capAnswer;
 }
 
 // create random character array 
 function randomChar() {
-  var charArr = ("!@#$%^&*()_-=+")
-  return charArr[Math.floor(Math.random() * charArr.length)]
+  var charArr = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+"]
+  for (var i = 0; i < charQuestion; i++) {
+    capAnswer += charArr[Math.floor(Math.random() * charArr.length)]
+  }
+  return capAnswer;
 }
 
 // random lower case letter array 
 function randomLowerLetter() {
-  var lowerLetterArr = ("abcdefghijklmnopqrstuvwxyz")
-  return lowerLetterArr[Math.floor(Math.random() * lowerLetterArr.length)]
+  var lowerLetterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  for (var i = 0; i < charQuestion; i++) {
+    capAnswer += lowerLetterArr[Math.floor(Math.random() * lowerLetterArr.length)]
+  }
+  return capAnswer;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//click event result + password generator
+generateBtn.addEventListener("click", function (event) {
+
+  var confirmArr = [
+    confirm("Would you like to use capital letters?"),
+    confirm("Would you like to use numbers"),
+    confirm("Would you like to use lower case letters?"),
+    confirm("Would you like to use special characters?")
+  ]
+
+  // confirmArr[0] == randomCapLetter();
+  // confirmArr[1] == randomNum();
+  // confirmArr[2] == randomChar();
+  // confirmArr[3] == randomLowerLetter();
+
+  console.log(confirmArr[0]);
+  console.log(confirmArr[1]);
+  console.log(confirmArr[2]);
+  console.log(confirmArr[3]);
+  // var answer = '';
+
+  if (confirmArr[0]) {
+    confirmArr[0] == randomCapLetter();
+    // var answer = '';
+    // console.log(capAnswer);
+    // for (var i = 0; i < charQuestion; i++) {
+    //   answer += capAnswer.charAt(Math.floor(Math.random() * charQuestion * 2))
+    // }
+    // passwordText.value = answer;
+  }
+  if (confirmArr[1] == true) {
+    confirmArr[1] == randomNum();
+    // var answer = '';
+    // console.log(capAnswer);
+    // for (var i = 0; i < charQuestion; i++) {
+    //   answer += capAnswer.charAt(Math.floor(Math.random() * charQuestion * 2))
+    // }
+    // passwordText.value = answer;
+  }
+  if (confirmArr[2] == true) {
+    confirmArr[2] == randomChar();
+    // var answer = '';
+    // console.log(capAnswer);
+    // for (var i = 0; i < charQuestion; i++) {
+    //   answer += capAnswer.charAt(Math.floor(Math.random() * charQuestion * 2))
+    // }
+    // passwordText.value = answer;
+  }
+  if (confirmArr[3] == true) {
+    confirmArr[3] == randomLowerLetter();
+  };
+
+    var answer = '';
+    console.log(capAnswer);
+    for (var i = 0; i < charQuestion; i++) {
+      answer += capAnswer.charAt(Math.floor(Math.random()))
+    }
+    passwordText.value = answer * charQuestion;
+  
+  
+
+
+
+
+  // if (confirmArr[2] === false && confirmArr[3] === false) {
+  //   randomCapLetter();
+  //   randomNum();
+  //   var answer = '';
+  //   console.log(capAnswer);
+  //   for (var i = 0; i < charQuestion; i++) {
+  //     answer += capAnswer.charAt(Math.floor(Math.random() * charQuestion * 2))
+  //   }
+  //   passwordText.value = answer;
+  // };
+
+  // if false
+  // randomCapLetter() === confirmArr[0];
+  // randomNum() === confirmArr[1];
+  // randomChar() === confirmArr[2];
+  // randomLowerLetter() === confirmArr[3];
+
+//   for (i = 0; i < confirmArr; i++); {
+// confirmArr;
+//     if (confirmArr[i] == true) {
+//       console.log (indexOf.confirmArr);
+//       var answer = '';
+//       console.log(capAnswer);
+//       for (var i = 0; i < charQuestion; i++) {
+//         answer += capAnswer.charAt(Math.floor(Math.random() * charQuestion * 2))
+//       }
+//       return (capAnswer);
+//     };
+//   };
+
+  //   if (confirmArr[i]) {
+  //     randomCapLetter();
+  //     randomNum();
+  //     randomChar();
+  //     randomLowerLetter();
+  //     var answer = '';
+  //     console.log(capAnswer);
+  //     for (var i = 0; i < charQuestion; i++) {
+  //       answer += capAnswer.charAt(Math.floor(Math.random() * charQuestion * 2))
+  //     }
+  //     passwordText.value = answer;
+  //   } if (confirmArr[i] === false) {
+  //     return confirmArr[i];
+  //   }
+  //   else {
+  //     console.log("error");
+// }
+});
+
